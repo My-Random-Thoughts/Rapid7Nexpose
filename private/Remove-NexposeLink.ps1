99c297a5b2a35a0af132bfb5d9e1655936744f69
My Random Thoughts <support@myrandomthoughts.co.uk>
2019-09-24 09:30:02 +0100

    Moved ShouldProcess check to fix bug

--- private/Remove-NexposeLink.ps1
+++ private/Remove-NexposeLink.ps1
@@ -25,17 +25,17 @@ Function Remove-NexposeLink {
         [object[]]$InputObject
     )
 
-    If ($PSCmdlet.ShouldProcess('InputObject')) {
-        ForEach ($obj In $InputObject) {
-            # Check to see if there is anything other than links
-            [string[]]$pNames = (($obj | Get-Member -MemberType *Property).Name)
-            If (($pNames.Count -eq 1) -and ($pNames[0] -eq 'links')) {
-                Write-Output $obj
-            }
-            Else {
-                # Remove top level links
+    ForEach ($obj In $InputObject) {
+        # Check to see if there is anything other than links
+        [string[]]$pNames = (($obj | Get-Member -MemberType *Property).Name)
+        If (($pNames.Count -eq 1) -and ($pNames[0] -eq 'links')) {
+            Write-Output $obj
+        }
+        Else {
+            # Remove top level links
+            If ($PSCmdlet.ShouldProcess('InputObject')) {
                 If ($obj.links) { $obj = ($obj | Select-Object -Property * -ExcludeProperty 'links') }
-
+    
                 # Remove sub-level links
                 $obj | Get-Member -MemberType *Property | `
                     ForEach-Object -Process {
@@ -44,9 +44,9 @@ Function Remove-NexposeLink {
                             $obj.$pName = ($obj.$pName | Select-Object -Property * -ExcludeProperty 'links')
                         }
                     }
-
-                Write-Output $obj
             }
+
+            Write-Output $obj
         }
     }
 }
