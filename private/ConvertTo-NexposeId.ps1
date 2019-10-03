505d41aa5c272b3cb322b3fa582e6a81af1092c0
My Random Thoughts <support@myrandomthoughts.co.uk>
2019-10-03 13:42:21 +0100

    Added Credential convert

--- private/ConvertTo-NexposeId.ps1
+++ private/ConvertTo-NexposeId.ps1
@@ -10,7 +10,7 @@ Function ConvertTo-NexposeId {
         The name of the object
 
     .PARAMETER ObjectType
-        The type of the tag being specified.  One of 'AuthSource', 'Asset', 'AssetGroup', 'Report', 'Policy', 'Site', 'Tag', 'User'
+        The type of the tag being specified.  One of 'AuthSource', 'Asset', 'AssetGroup', 'Credential', 'Report', 'Policy', 'Site', 'Tag', 'User'
 
     .EXAMPLE
         ConvertTo-NexposeId -Name 'asset_1' -ObjectType 'Asset'
@@ -19,7 +19,7 @@ Function ConvertTo-NexposeId {
         ConvertTo-NexposeId -Name 'tag_1' -ObjectType 'Tag'
 
     .NOTES
-        For additional information please see my GitHub wiki page
+        For additonal information please contact PlatformBuild@callcreditgroup.com
 
     .LINK
         https://github.com/My-Random-Thoughts/Rapid7Nexpose
@@ -31,7 +31,7 @@ Function ConvertTo-NexposeId {
         [string]$Name,
 
         [Parameter(Mandatory = $true)]
-        [ValidateSet('AuthSource','Asset','AssetGroup','Report','Policy','Site','Tag','User')]
+        [ValidateSet('AuthSource','Asset','AssetGroup','Credential','Report','Policy','ScanEnginePool','Site','Tag','User')]
         [string]$ObjectType
     )
 
@@ -44,13 +44,37 @@ Function ConvertTo-NexposeId {
                 If ([ipaddress]::TryParse($Name, [ref]$null)) { $result = ((Get-NexposeAsset -IpAddress $Name).id) }
                 Else                                          { $result = ((Get-NexposeAsset -Name      $Name).id) }
             }
-            'AssetGroup' { $result = (((Invoke-NexposeQuery -UrlFunction 'asset_groups'                           -RestMethod Get) | Where-Object {  $_.name -eq $Name                          }).id) }
-            'AuthSource' { $result = (((Invoke-NexposeQuery -UrlFunction 'authentication_sources'                 -RestMethod Get) | Where-Object {  $_.type -eq $Name                          }).id) }
-            'Policy'     { $result = (((Invoke-NexposeQuery -UrlFunction 'policies' -ApiQuery @{ filter = $Name } -RestMethod Get) | Select-Object -First 1                                      ).id) }
-            'Report'     { $result = (((Invoke-NexposeQuery -UrlFunction 'reports'                                -RestMethod Get) | Where-Object {  $_.name -eq $Name                          }).id) }
-            'Site'       { $result = (((Invoke-NexposeQuery -UrlFunction 'sites'                                  -RestMethod Get) | Where-Object {  $_.name -eq $Name                          }).id) }
-            'Tag'        { $result = (((Invoke-NexposeQuery -UrlFunction 'tags'                                   -RestMethod Get) | Where-Object {  $_.name -eq $Name                          }).id) }
-            'User'       { $result = (((Invoke-NexposeQuery -UrlFunction 'users'                                  -RestMethod Get) | Where-Object { ($_.name -eq $Name) -or ($_.login -eq $Name)}).id) }
+
+            'Credential' {
+                $sharedcred = (((Invoke-NexposeQuery -UrlFunction 'shared_credentials' -RestMethod Get) | Where-Object {  $_.name -eq $Name }).id)
+
+                If (-not $sharedcred) {
+                    ForEach ($site In (Get-NexposeSite)) {
+                        $sitecred = (((Invoke-NexposeQuery -UrlFunction "sites/$($site.id)/site_credentials" -RestMethod Get) | Where-Object {  $_.name -eq $Name }).id)
+                        If ($sitecred) {
+                            $result = $sitecred
+                        }
+                    }
+                }
+                Else {
+                    $result = $sharedcred
+                }
+            }
+
+            'Policy' { $result = (((Invoke-NexposeQuery -UrlFunction 'policies' -ApiQuery @{ filter = $Name } -RestMethod Get) | Select-Object -First 1).id) }
+            'User'   { $result = (((Invoke-NexposeQuery -UrlFunction 'users'                                  -RestMethod Get) | Where-Object { ($_.name -eq $Name) -or ($_.login -eq $Name)}).id) }
+
+            Default {
+                Switch ($ObjectType) {
+                    'AssetGroup'     { $url = 'asset_groups';           $obj = 'name' }
+                    'AuthSource'     { $url = 'authentication_sources'; $obj = 'type' }
+                    'Report'         { $url = 'reports';                $obj = 'name' }
+                    'ScanEnginePool' { $url = 'scan_engine_pools';      $obj = 'name' }
+                    'Site'           { $url = 'sites';                  $obj = 'name' }
+                    'Tag'            { $url = 'tags';                   $obj = 'name' }
+                }
+                $result = (((Invoke-NexposeQuery -UrlFunction $url -RestMethod Get) | Where-Object {  $_.$obj -eq $Name }).id)
+            }
         }
     }
     Else {
